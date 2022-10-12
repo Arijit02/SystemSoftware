30d8d7e63b0c19fe9ee5b4fd4c0a33addfd31dc1
Soumya Prabha Maiti <soumyaprabhamaiti2001@gmail.com>
2022-10-12 22:01:30 +0530

    Added Round Robin algorithm

--- OS/CPU Scheduling/CPU_scheduling.cpp
+++ OS/CPU Scheduling/CPU_scheduling.cpp
@@ -111,19 +111,64 @@ public:
         display(processList);
     }
 };
+class RR : public ProcessScheduling
+{
+public:
+    void run(vector<Process> processList, int timeQuantum)
+    {
+        sort(processList.begin(), processList.end(), sortIncreasingArrivalTime);
+        int time = processList[0].arrivalTime;
+
+        queue<int> waitingQ; // Queue containing those indices of processList array which have already arrived
+        waitingQ.push(0);
+
+        while (!waitingQ.empty())
+        {
+            int currProcessIndex = waitingQ.front();
+            waitingQ.pop();
+
+            int runtime = min(processList[currProcessIndex].remBurstTime, timeQuantum);
+            // cout<<"Executing "<<processList[currProcessIndex].pID<<endl;
+            processList[currProcessIndex].remBurstTime -= runtime;
+            time += runtime;
+
+            for (int i = currProcessIndex + 1; i < processList.size(); i++)
+            {
+                if (processList[i].arrivalTime <= time && processList[i].arrivalTime > time - runtime)
+                    waitingQ.push(i);
+            }
+            if (processList[currProcessIndex].remBurstTime)
+                waitingQ.push(currProcessIndex);
+            else
+                processList[currProcessIndex].completionTime = time;
+        }
+        for (int i = 0; i < processList.size(); i++)
+        {
+            processList[i].turnAroundTime = processList[i].completionTime - processList[i].arrivalTime;
+            processList[i].waitingTime = processList[i].turnAroundTime - processList[i].burstTime;
+        }
+
+        display(processList);
+    }
+};
 
 int main()
 {
 
-    vector<Process> processList = {{1, 0, 5}, {2, 3, 9}, {3, 6, 6}};
+    vector<Process> processList1 = {{1, 0, 5}, {2, 3, 9}, {3, 6, 6}};
 
     FCFS f;
-    f.run(processList);
+    f.run(processList1);
 
     vector<Process> processList2 = {{1, 1, 7}, {2, 2, 5}, {3, 3, 1}, {4, 4, 2}, {5, 5, 8}};
 
     SJF s;
     s.run(processList2);
+
+    vector<Process> processList2 = {{1, 0, 5}, {2, 1, 4}, {3, 2, 2}, {4, 3, 1}};
+
+    RR r;
+    r.run(processList2, 2);
 
     return 0;
 }
